a37a68cce7f965ce871b1d1a36aca1a0ab05eec3
Tiago Vaz <vaz@T4-5D.t4.srv.br>
2021-04-20 09:52:24 -0300

    Welcome screen done

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading'
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+
 import { Welcome } from './src/pages/Welcome';
 
+
 export default function App() {
+  // Necessário fazer no app para que as fontes fiquem disponíveis em toda aplicação
+  // useFonts é um HOOK, primeiro item do array é um booleano
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  // Segura a tela de loading enquanto as fonts são carregadas
+  if(!fontsLoaded)
+    return <AppLoading />
+
   return (
     <Welcome />
   )
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,43 +1,72 @@
 import React from 'react';
-import { Text, Image, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { 
+  Text, 
+  Image, 
+  StyleSheet, 
+  TouchableOpacity, 
+  Dimensions, 
+  View
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather } from '@expo/vector-icons';
 
 
-import wateringImg from '../assets/watering.png'
-import { Button } from '../Components/Button';
+import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-export function Welcome(){
-  return(
+export function Welcome() {
+  return (
     <SafeAreaView style={styles.container}>
-
+      <View style={styles.wrapper}>
+        
         <Text style={styles.title}>
           Gerencie {'\n'}
-          suas plantas {'\n'}
-          de forma fácil
+          suas plantas de {'\n'}
+          forma fácil
         </Text>
 
-        <Image source={wateringImg} />
+        <Image 
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
 
         <Text style={styles.subtitle}>
           Lembre sempre de regar as platinhas camarada!
         </Text>
 
-      <Button title='>' />
+        <TouchableOpacity 
+          style={styles.button} 
+          activeOpacity={0.7} 
+        >
 
+          <Feather 
+            name='chevron-right' 
+            style={styles.buttonIcon} 
+          />
+
+        </TouchableOpacity>
+
+      </View>
     </SafeAreaView>
   )
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
@@ -45,13 +74,26 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20
   },
   subtitle: {
+    fontFamily: fonts.text,
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading
   },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    width: 56,
+    height: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 32
+  },
   image: {
-    height: 292,
-    width: 284
+    height: Dimensions.get(`window`).width * 0.7, // Calcula baseado no device. Também metodo na tag imagem
   }
 })
